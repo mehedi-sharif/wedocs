09e865b44a46384dcc6f1daa891bb715dca77d6c
Mehedi Sharif <mehedishariftitas@gmail.com>
2017-09-25 01:38:25 +0600

    Added screenshot.png file

--- templates/footer.php
+++ templates/footer.php
@@ -2,21 +2,16 @@
     <div class="container">
         <div class="row">
 
-            <div class="widget-area clearfix">
-                <?php dynamic_sidebar('sidebar-footer'); ?>
-            </div>
-
-            <div class="col-md-12 copyright clearfix">
+            <div class="col-md-12 copyright text-center">
                 <span class="copy-text">
                     <?php if ( $footer_text = get_theme_mod( 'wedocs_footer_text' ) ) {
                         echo esc_html( $footer_text );
                     } else { ?>
-                        &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
+                        &copy; <?php echo date('Y'); ?><?php bloginfo('name'); ?>
                     <?php } ?>
                 </span>
 
-                <?php $credit = get_theme_mod( 'wedocs_footer_credit', true ); ?>
-                <?php do_action( 'wedocs_credits' ); ?>
+                
             </div>
         </div>
     </div>
