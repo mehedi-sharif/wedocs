5b7b9cc421908db25fb1e3971e36dcb06362778f
Mehedi Sharif <mehedishariftitas@gmail.com>
2017-09-27 15:45:33 +0600

    Update markup

--- wedocs/shortcode.php
+++ wedocs/shortcode.php
@@ -4,23 +4,25 @@
     <div class="wedocs-docs-list row">
 
         <?php foreach ($docs as $main_doc) : ?>
-            <div class="col-md-4 wedocs-docs-single">
-                <h3><a href="<?php echo get_permalink( $main_doc['doc']->ID ); ?>"><?php echo $main_doc['doc']->post_title; ?></a></h3>
+            <div class="col-md-4">
+                <div class="wedocs-docs-single">
+                    <h3><a href="<?php echo get_permalink( $main_doc['doc']->ID ); ?>"><?php echo $main_doc['doc']->post_title; ?></a></h3>
 
-                <?php if ( $main_doc['sections'] ) : ?>
+                    <?php if ( $main_doc['sections'] ) : ?>
 
-                    <div class="inside">
-                        <ul class="wedocs-doc-sections">
-                            <?php foreach ($main_doc['sections'] as $section) : ?>
-                                <li><a href="<?php echo get_permalink( $section->ID ); ?>"><?php echo $section->post_title; ?></a></li>
-                            <?php endforeach; ?>
-                        </ul>
+                        <div class="inside">
+                            <ul class="wedocs-doc-sections">
+                                <?php foreach ($main_doc['sections'] as $section) : ?>
+                                    <li><a href="<?php echo get_permalink( $section->ID ); ?>"><?php echo $section->post_title; ?></a></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+
+                    <?php endif; ?>
+
+                    <div class="wedocs-doc-link">
+                        <a href="<?php echo get_permalink( $main_doc['doc']->ID ); ?>"><?php echo $more; ?></a>
                     </div>
-
-                <?php endif; ?>
-
-                <div class="wedocs-doc-link">
-                    <a href="<?php echo get_permalink( $main_doc['doc']->ID ); ?>"><?php echo $more; ?></a>
                 </div>
             </div>
         <?php endforeach; ?>
