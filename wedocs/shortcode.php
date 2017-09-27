3ba1ce7f9329c4d8ba9059a67a4acc970c7253b5
Mehedi Sharif <mehedishariftitas@gmail.com>
2017-09-27 15:22:33 +0600

    Change shortcode layout style

---
+++ wedocs/shortcode.php
@@ -0,0 +1,30 @@
+<?php if ( $docs ) : ?>
+
+<div class="wedocs-shortcode-wrap">
+    <div class="wedocs-docs-list col-<?php echo $col; ?>">
+
+        <?php foreach ($docs as $main_doc) : ?>
+            <div class="wedocs-docs-single- col-md-3">
+                <h3><a href="<?php echo get_permalink( $main_doc['doc']->ID ); ?>"><?php echo $main_doc['doc']->post_title; ?></a></h3>
+
+                <?php if ( $main_doc['sections'] ) : ?>
+
+                    <div class="inside">
+                        <ul class="wedocs-doc-sections">
+                            <?php foreach ($main_doc['sections'] as $section) : ?>
+                                <li><a href="<?php echo get_permalink( $section->ID ); ?>"><?php echo $section->post_title; ?></a></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
+                <?php endif; ?>
+
+                <div class="wedocs-doc-link">
+                    <a href="<?php echo get_permalink( $main_doc['doc']->ID ); ?>"><?php echo $more; ?></a>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+<?php endif;
